172fd51981de0aeedf28e46760f541e9f712a979
07Owuor <tnowuor4@gmail.com>
2023-05-10 11:04:33 +0300

    Fixed whitenoise

--- VendorPortal/settings.py
+++ VendorPortal/settings.py
@@ -145,7 +145,8 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static_root")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
